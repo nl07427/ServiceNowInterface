a0fc1a2e50e2348b2746565783d0e1687e0fd89e
Gedeon d' Abreu de Paulo <g.dabreudepaulo-1@student.tudelft.nl>
2022-11-29 00:05:01 +0100

    Add DerivedType and AbstractClass deserialization tests and AbstractClass TestModel

---
+++ tests/ServiceNow.Graph.Test/Serialization/SerializerTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using ServiceNow.Graph.Exceptions;
+using ServiceNow.Graph.Serialization;
+using ServiceNow.Graph.Test.TestModels;
+using Xunit;
+
+namespace ServiceNow.Graph.Test.Serialization
+{
+    public class SerializerTests
+    {
+        private Serializer serializer;
+
+        public SerializerTests()
+        {
+            this.serializer = new Serializer();
+        }
+
+        [Fact]
+        public void AbstractClassDeserializationFailure()
+        {
+            var stringToDeserialize = "{\"jsonKey\":\"jsonValue\"}";
+            JsonSerializationException exception = Assert.Throws<JsonSerializationException>(() => this.serializer.DeserializeObject<AbstractClass>(stringToDeserialize));
+        }
+
+        [Fact]
+        public void DeserializeDerivedType()
+        {
+            var id = "id";
+            var name = "name";
+
+            var stringToDeserialize = string.Format(
+                "{{\"id\":\"{0}\", \"@odata.type\":\"#microsoft.graph.dotnetCore.core.test.testModels.derivedTypeClass\", \"name\":\"{1}\"}}",
+                id,
+                name);
+
+            var derivedType = this.serializer.DeserializeObject<DerivedTypeClass>(stringToDeserialize);
+
+            Assert.NotNull(derivedType);
+            Assert.Equal(id, derivedType.Id);
+            Assert.Equal(name, derivedType.Name);
+        }
+    }
+}
---
+++ tests/ServiceNow.Graph.Test/TestModels/AbstractClass.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using ServiceNow.Graph.Serialization;
+
+namespace ServiceNow.Graph.Test.TestModels
+{
+    /// <summary>
+    /// A property bag class with no default constructor for unit testing purposes.
+    /// </summary>
+    [JsonConverter(typeof(DerivedTypeConverter))]
+    public abstract class AbstractClass
+    {
+        protected AbstractClass()
+        {
+
+        }
+
+        /// <summary>
+        /// Gets or sets id.
+        /// </summary>
+        [JsonPropertyName("id")]
+        public string Id
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Gets or sets additional data.
+        /// </summary>
+        [JsonExtensionData]
+        public IDictionary<string, object> AdditionalData
+        {
+            get; set;
+        }
+    }
+}
