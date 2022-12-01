90504b6d780eeb06f44da22a84163ace1eaa1ca9
Gedeon d' Abreu de Paulo <g.dabreudepaulo-1@student.tudelft.nl>
2022-12-01 16:25:34 +0100

    Add ClientCredentialProvider closed port request test

---
+++ tests/ServiceNow.Graph.Test/Authentication/ClientCredentialProviderTests.cs
@@ -0,0 +1,24 @@
+﻿using ServiceNow.Graph.Authentication;
+using ServiceNow.Graph.Exceptions;
+using System;
+using Xunit;
+
+namespace ServiceNow.Graph.Test.Authentication
+{
+    public class ClientCredentialProviderTests
+    {
+        [Fact]
+        public async void AuthenticateRequestWithClosedPortThrowsException()
+        {
+            string domain = "127.0.0.1";
+            string clientId = "00000000-0000-0000-0000-000000000000";
+            string clientSecret = "00000000-0000-0000-0000-000000000000";
+            string username = "user";
+            string password = "password";
+
+            ClientCredentialProvider credentialProvider = new ClientCredentialProvider(domain, clientId, clientSecret, username, password);
+
+            AuthenticationException ex = await Assert.ThrowsAsync<AuthenticationException>(() => credentialProvider.AuthenticateRequestAsync(null));
+        }
+    }
+}
