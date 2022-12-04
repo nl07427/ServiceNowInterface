27756f9345e77b87eb82a5a7baa8c9d3d3991890
Gedeon d' Abreu de Paulo <g.dabreudepaulo-1@student.tudelft.nl>
2022-12-04 22:34:51 +0100

    Add ServiceNowClientFactory pipeline, create client and send request tests

---
+++ tests/ServiceNow.Graph.Test/Mocks/MockRedirectHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServiceNow.Graph.Test.Mocks
+{
+    public class MockRedirectHandler : HttpMessageHandler
+    {
+        private HttpResponseMessage _response1
+        {
+            get; set;
+        }
+        private HttpResponseMessage _response2
+        {
+            get; set;
+        }
+
+        private bool _response1Sent = false;
+
+        protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (!_response1Sent)
+            {
+                _response1Sent = true;
+                _response1.RequestMessage = request;
+                return await Task.FromResult(_response1);
+            }
+            else
+            {
+                _response1Sent = false;
+                _response2.RequestMessage = request;
+                return await Task.FromResult(_response2);
+            }
+        }
+
+        public void SetHttpResponse(HttpResponseMessage response1, HttpResponseMessage response2 = null)
+        {
+            this._response1Sent = false;
+            this._response1 = response1;
+            this._response2 = response2;
+        }
+    }
+}
---
+++ tests/ServiceNow.Graph.Test/Requests/ServiceNowClientFactoryTests.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using ServiceNow.Graph.Requests;
+using ServiceNow.Graph.Requests.Middleware;
+using ServiceNow.Graph.Test.Mocks;
+using Xunit;
+
+namespace ServiceNow.Graph.Test.Requests
+{
+    public class GraphClientFactoryTests : IDisposable
+    {
+        private MockRedirectHandler testHttpMessageHandler;
+        private DelegatingHandler[] handlers;
+        private const string expectedAccessToken = "service-now-client-factory-infused-token";
+        private MockAuthenticationProvider testAuthenticationProvider;
+        private string domain = "127.0.0.1";
+
+        public GraphClientFactoryTests()
+        {
+            this.testHttpMessageHandler = new MockRedirectHandler();
+            testAuthenticationProvider = new MockAuthenticationProvider(expectedAccessToken);
+            handlers = ServiceNowClientFactory.CreateDefaultHandlers(testAuthenticationProvider.Object).ToArray();
+        }
+
+        public void Dispose()
+        {
+            this.testHttpMessageHandler.Dispose();
+        }
+
+        // Note:
+        // 1. Xunit's IsType doesn't consider inheritance behind the classes.
+        // 2. We can't control the order of execution for the tests
+        // and 'ServiceNowClientFactory.DefaultHttpHandler' can easily be modified
+        // by other tests since it's a static delegate.
+
+#if iOS || macOS
+        [Fact]
+        public void Should_CreatePipeline_Without_CompressionHandler()
+        {
+            using (AuthenticationHandler authenticationHandler = (AuthenticationHandler)ServiceNowClientFactory.CreatePipeline(handlers))
+            using (RetryHandler retryHandler = (RetryHandler)authenticationHandler.InnerHandler)
+            using (RedirectHandler redirectHandler = (RedirectHandler)retryHandler.InnerHandler)
+#if iOS
+            using (NSUrlSessionHandler innerMost = (NSUrlSessionHandler)redirectHandler.InnerHandler)
+#elif macOS
+            using (Foundation.NSUrlSessionHandler innerMost = (Foundation.NSUrlSessionHandler)redirectHandler.InnerHandler)
+#endif
+            {
+                Assert.NotNull(authenticationHandler);
+                Assert.NotNull(retryHandler);
+                Assert.NotNull(redirectHandler);
+                Assert.NotNull(innerMost);
+                Assert.IsType<AuthenticationHandler>(authenticationHandler);
+                Assert.IsType<RetryHandler>(retryHandler);
+                Assert.IsType<RedirectHandler>(redirectHandler);
+#if iOS
+                Assert.IsType<NSUrlSessionHandler>(innerMost);
+#elif macOS
+                Assert.IsType<Foundation.NSUrlSessionHandler>(innerMost);
+#endif
+            }
+        }
+#else
+        [Fact]
+        public void Should_CreatePipeline_Without_HttpMessageHandlerInput()
+        {
+            using (AuthenticationHandler authenticationHandler = (AuthenticationHandler)ServiceNowClientFactory.CreatePipeline(handlers))
+            using (CompressionHandler compressionHandler = (CompressionHandler)authenticationHandler.InnerHandler)
+            using (RetryHandler retryHandler = (RetryHandler)compressionHandler.InnerHandler)
+            using (RedirectHandler redirectHandler = (RedirectHandler)retryHandler.InnerHandler)
+            using (HttpMessageHandler innerMost = redirectHandler.InnerHandler)
+            {
+                Assert.NotNull(authenticationHandler);
+                Assert.NotNull(compressionHandler);
+                Assert.NotNull(retryHandler);
+                Assert.NotNull(redirectHandler);
+                Assert.NotNull(innerMost);
+                Assert.IsType<AuthenticationHandler>(authenticationHandler);
+                Assert.IsType<CompressionHandler>(compressionHandler);
+                Assert.IsType<RetryHandler>(retryHandler);
+                Assert.IsType<RedirectHandler>(redirectHandler);
+                Assert.True(innerMost is HttpMessageHandler);
+            }
+        }
+#endif
+        [Fact]
+        public void CreatePipelineWithHttpMessageHandlerInput()
+        {
+            using (AuthenticationHandler authenticationHandler = (AuthenticationHandler)ServiceNowClientFactory.CreatePipeline(handlers, this.testHttpMessageHandler))
+            using (CompressionHandler compressionHandler = (CompressionHandler)authenticationHandler.InnerHandler)
+            using (RetryHandler retryHandler = (RetryHandler)compressionHandler.InnerHandler)
+            using (RedirectHandler redirectHandler = (RedirectHandler)retryHandler.InnerHandler)
+            using (MockRedirectHandler innerMost = (MockRedirectHandler)redirectHandler.InnerHandler)
+            {
+                Assert.NotNull(authenticationHandler);
+                Assert.NotNull(compressionHandler);
+                Assert.NotNull(retryHandler);
+                Assert.NotNull(redirectHandler);
+                Assert.NotNull(innerMost);
+                Assert.IsType<AuthenticationHandler>(authenticationHandler);
+                Assert.IsType<CompressionHandler>(compressionHandler);
+                Assert.IsType<RetryHandler>(retryHandler);
+                Assert.IsType<RedirectHandler>(redirectHandler);
+                Assert.IsType<MockRedirectHandler>(innerMost);
+            }
+        }
+
+        [Fact]
+        public void CreatePipelineWithoutPipeline()
+        {
+            using (MockRedirectHandler handler = (MockRedirectHandler)ServiceNowClientFactory.CreatePipeline(null, this.testHttpMessageHandler))
+            {
+                Assert.NotNull(handler);
+                Assert.IsType<MockRedirectHandler>(handler);
+            }
+        }
+
+        [Fact]
+        public void CreatePipeline_Should_Throw_Exception_With_Duplicate_Handlers()
+        {
+            var handlers = ServiceNowClientFactory.CreateDefaultHandlers(testAuthenticationProvider.Object);
+            handlers.Add(new AuthenticationHandler(testAuthenticationProvider.Object));
+
+            ArgumentException exception = Assert.Throws<ArgumentException>(() => ServiceNowClientFactory.CreatePipeline(handlers));
+
+            Assert.Contains($"{typeof(AuthenticationHandler)} has a duplicate handler.", exception.Message);
+        }
+
+        [Fact]
+        public void CreateClient_CustomHttpHandlingBehaviors()
+        {
+            var timeout = TimeSpan.FromSeconds(200);
+            var baseAddress = new Uri($"https://{this.domain}/api");
+            var cacheHeader = new CacheControlHeaderValue();
+
+            using (HttpClient client = ServiceNowClientFactory.Create(testAuthenticationProvider.Object, this.domain))
+            {
+                client.Timeout = timeout;
+                client.BaseAddress = baseAddress;
+                Assert.NotNull(client);
+                Assert.Equal(timeout, client.Timeout);
+                Assert.Equal(baseAddress, client.BaseAddress);
+            }
+        }
+
+        [Fact]
+        public void CreateClient_WithInnerHandler()
+        {
+            using (HttpClient httpClient = ServiceNowClientFactory.Create(authenticationProvider: testAuthenticationProvider.Object, domain: this.domain, finalHandler: this.testHttpMessageHandler))
+            {
+                Assert.NotNull(httpClient);
+                Assert.True(httpClient.DefaultRequestHeaders.Contains(Constants.Headers.SdkVersionHeaderName), "SDK version not set.");
+                Version assemblyVersion = typeof(ServiceNowClientFactory).GetTypeInfo().Assembly.GetName().Version;
+                string value = string.Format(
+                    Constants.Headers.SdkVersionHeaderValueFormatString,
+                    "ServiceNowRestApi",
+                    assemblyVersion.Major,
+                    assemblyVersion.Minor,
+                    assemblyVersion.Build);
+                IEnumerable<string> values;
+                Assert.True(httpClient.DefaultRequestHeaders.TryGetValues(Constants.Headers.SdkVersionHeaderName, out values), "SDK version value not set");
+                Assert.Single(values);
+                Assert.Equal(value, values.First());
+            }
+        }
+
+        [Fact]
+        public void CreateClient_WithHandlers()
+        {
+            using (HttpClient client = ServiceNowClientFactory.Create(handlers: ServiceNowClientFactory.CreateDefaultHandlers(testAuthenticationProvider.Object), domain: this.domain))
+            {
+                Assert.NotNull(client);
+            }
+        }
+
+        [Fact]
+        public async Task SendRequest_Redirect()
+        {
+            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, "http://example.org/foo");
+            var redirectResponse = new HttpResponseMessage(HttpStatusCode.MovedPermanently);
+
+            redirectResponse.Headers.Location = new Uri("http://example.org/bar");
+            var oKResponse = new HttpResponseMessage(HttpStatusCode.OK);
+            this.testHttpMessageHandler.SetHttpResponse(redirectResponse, oKResponse);
+
+            using (HttpClient client = ServiceNowClientFactory.Create(authenticationProvider: testAuthenticationProvider.Object, domain: this.domain, finalHandler: this.testHttpMessageHandler))
+            {
+                var response = await client.SendAsync(httpRequestMessage, new CancellationToken());
+                Assert.Equal(oKResponse, response);
+                Assert.Equal(httpRequestMessage.Method, response.RequestMessage.Method);
+                Assert.NotSame(httpRequestMessage, response.RequestMessage);
+            }
+
+        }
+
+        [Fact]
+        public async Task SendRequest_Retry()
+        {
+            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, "http://example.org/foo");
+            httpRequestMessage.Content = new StringContent("Hello World");
+
+            var retryResponse = new HttpResponseMessage(HttpStatusCode.ServiceUnavailable);
+            retryResponse.Headers.TryAddWithoutValidation("Retry-After", 2.ToString());
+            var response_2 = new HttpResponseMessage(HttpStatusCode.OK);
+
+            this.testHttpMessageHandler.SetHttpResponse(retryResponse, response_2);
+
+            using (HttpClient client = ServiceNowClientFactory.Create(authenticationProvider: testAuthenticationProvider.Object, domain: this.domain, finalHandler: this.testHttpMessageHandler))
+            {
+                var response = await client.SendAsync(httpRequestMessage, new CancellationToken());
+                Assert.Same(response_2, response);
+                IEnumerable<string> values;
+                Assert.True(response.RequestMessage.Headers.TryGetValues("Retry-Attempt", out values), "Don't set Retry-Attemp Header");
+                Assert.Single(values);
+                Assert.Equal(1.ToString(), values.First());
+                Assert.NotSame(httpRequestMessage, response.RequestMessage);
+            }
+        }
+    }
+}
