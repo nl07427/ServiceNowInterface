78474fc3c631845c560f1ee3bff6561a8586b103
Gedeon d' Abreu de Paulo <g.dabreudepaulo-1@student.tudelft.nl>
2022-12-04 23:24:17 +0100

    Add ResponseHandlerTests

---
+++ tests/ServiceNow.Graph.Test/Requests/ResponseHandlerTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceNow.Graph.Requests;
+using ServiceNow.Graph.Serialization;
+using ServiceNow.Graph.Test.TestModels.ServiceModels;
+using Xunit;
+
+namespace ServiceNow.Graph.Test.Requests
+{
+    public class ResponseHandlerTests
+    {
+        [Fact]
+        public async Task HandleUserResponse()
+        {
+            // Arrange
+            var responseHandler = new ResponseHandler(new Serializer());
+            var hrm = new HttpResponseMessage()
+            {
+                Content = new StringContent(@"{
+                    ""id"": ""123"",
+                    ""givenName"": ""Joe"",
+                    ""surName"": ""Brown"",
+                    ""@odata.type"":""test""
+                }", Encoding.UTF8, "application/json")
+            };
+            hrm.Headers.Add("test", "value");
+
+            // Act
+            var user = await responseHandler.HandleResponse<TestUser>(hrm);
+
+            //Assert
+            Assert.Equal("123", user.Id);
+            Assert.Equal("Joe", user.GivenName);
+            Assert.Equal("Brown", user.Surname);
+        }
+    }
+}
