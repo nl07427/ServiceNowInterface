f8e3f5cf9d00bf167098b7e0272ceb300f327f31
Gedeon d' Abreu de Paulo <g.dabreudepaulo-1@student.tudelft.nl>
2022-12-05 17:42:25 +0100

    Add SimpleHttpProvider SendAsync header verification and exception throwing tests

--- tests/ServiceNow.Graph.Test/Requests/SimpleHttpProviderTests.cs
+++ tests/ServiceNow.Graph.Test/Requests/SimpleHttpProviderTests.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
 using ServiceNow.Graph.Exceptions;
@@ -124,6 +127,114 @@ namespace ServiceNow.Graph.Test.Requests
                 Assert.Equal(ErrorConstants.Codes.GeneralException, exception.Error.ErrorDetail.Message);
                 Assert.Equal(ErrorConstants.Messages.UnexpectedExceptionOnSend, exception.Error.ErrorDetail.DetailedMessage);
                 Assert.Equal(clientException, exception.InnerException);
+            }
+        }
+
+        [Fact]
+        public async Task SendAsync_RethrowsTaskCancelledException()
+        {
+            using (var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, this.domain))
+            {
+                this.simpleHttpProvider.Dispose();
+
+                var message = "Task cancelled";
+                var clientException = new TaskCanceledException(message);
+                var defaultHandlers = ServiceNowClientFactory.CreateDefaultHandlers(authProvider.Object);
+                var httpClient = ServiceNowClientFactory.Create(handlers: defaultHandlers, domain: this.domain, finalHandler: new ExceptionHttpMessageHandler(clientException));
+                this.simpleHttpProvider = new SimpleHttpProvider(httpClient, this.domain, this.serializer.Object);
+
+                ServiceException exception = await Assert.ThrowsAsync<ServiceException>(async () => await this.simpleHttpProvider.SendAsync(
+                    httpRequestMessage, HttpCompletionOption.ResponseContentRead, CancellationToken.None));
+
+                Assert.Equal(ErrorConstants.Codes.Timeout, exception.Error.ErrorDetail.Message);
+                Assert.Equal(ErrorConstants.Messages.RequestTimedOut, exception.Error.ErrorDetail.DetailedMessage);
+
+            }
+        }
+
+        [Fact]
+        public async Task SendAsync_ThrowsServiceExceptionOnInvalidRedirectResponse()
+        {
+            using (var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, "https://localhost"))
+            using (var httpResponseMessage = new HttpResponseMessage())
+            {
+                httpResponseMessage.StatusCode = HttpStatusCode.Redirect;
+                httpResponseMessage.RequestMessage = httpRequestMessage;
+                this.testHttpMessageHandler.AddResponseMapping(httpRequestMessage.RequestUri.ToString(), httpResponseMessage);
+
+                ServiceException exception = await Assert.ThrowsAsync<ServiceException>(async () => await this.simpleHttpProvider.SendAsync(httpRequestMessage));
+                Assert.Equal(ErrorConstants.Codes.GeneralException, exception.Error.ErrorDetail.Message);
+                Assert.Equal(
+                    ErrorConstants.Messages.LocationHeaderNotSetOnRedirect,
+                    exception.Error.ErrorDetail.DetailedMessage);
+            }
+        }
+
+        [Fact]
+        public async Task SendAsync_VerifiesHeadersOnRedirect()
+        {
+            using (var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, "https://localhost"))
+            using (var redirectResponseMessage = new HttpResponseMessage())
+            using (var finalResponseMessage = new HttpResponseMessage())
+            {
+                httpRequestMessage.Headers.Add("testHeader", "testValue");
+
+                redirectResponseMessage.StatusCode = HttpStatusCode.Redirect;
+                redirectResponseMessage.Headers.Location = new Uri("https://localhost/redirect");
+                redirectResponseMessage.RequestMessage = httpRequestMessage;
+
+                this.testHttpMessageHandler.AddResponseMapping(httpRequestMessage.RequestUri.ToString(), redirectResponseMessage);
+                this.testHttpMessageHandler.AddResponseMapping(redirectResponseMessage.Headers.Location.ToString(), finalResponseMessage);
+
+                var returnedResponseMessage = await this.simpleHttpProvider.SendAsync(httpRequestMessage);
+
+                Assert.Equal(7, finalResponseMessage.RequestMessage.Headers.Count());
+
+                foreach (var header in httpRequestMessage.Headers)
+                {
+                    var actualValues = finalResponseMessage.RequestMessage.Headers.GetValues(header.Key);
+
+                    var enumerable = actualValues as string[] ?? actualValues.ToArray();
+                    Assert.Equal(enumerable.Length, header.Value.Count());
+
+                    foreach (var headerValue in header.Value)
+                    {
+                        Assert.Contains(headerValue, enumerable);
+                    }
+                }
+
+                Assert.Equal(finalResponseMessage, returnedResponseMessage);
+            }
+        }
+
+        [Fact]
+        public async Task SendAsync_TThrowsServiceExceptionOnMaxRedirects()
+        {
+            using (var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, "https://localhost"))
+            using (var redirectResponseMessage = new HttpResponseMessage())
+            using (var tooManyRedirectsResponseMessage = new HttpResponseMessage())
+            {
+                redirectResponseMessage.StatusCode = HttpStatusCode.Redirect;
+                redirectResponseMessage.Headers.Location = new Uri("https://localhost/redirect");
+                tooManyRedirectsResponseMessage.StatusCode = HttpStatusCode.Redirect;
+                tooManyRedirectsResponseMessage.Headers.Location = new Uri("https://localhost");
+
+                redirectResponseMessage.RequestMessage = httpRequestMessage;
+
+                this.testHttpMessageHandler.AddResponseMapping(httpRequestMessage.RequestUri.ToString(), redirectResponseMessage);
+                this.testHttpMessageHandler.AddResponseMapping(redirectResponseMessage.Headers.Location.ToString(), tooManyRedirectsResponseMessage);
+
+                httpRequestMessage.Headers.Authorization = new AuthenticationHeaderValue(Constants.Headers.Bearer, "ticket");
+
+                ServiceException exception = await Assert.ThrowsAsync<ServiceException>(async () => await this.simpleHttpProvider.SendAsync(
+                    httpRequestMessage,
+                    HttpCompletionOption.ResponseContentRead,
+                    CancellationToken.None));
+
+                Assert.Equal(ErrorConstants.Codes.TooManyRedirects, exception.Error.ErrorDetail.Message);
+                Assert.Equal(
+                    string.Format(ErrorConstants.Messages.TooManyRedirectsFormatString, "5"),
+                    exception.Error.ErrorDetail.DetailedMessage);
             }
         }
     }
