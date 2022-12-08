85f002b6ad688c209d1d4d9486904fef86eb7614
Gedeon d' Abreu de Paulo <g.dabreudepaulo-1@student.tudelft.nl>
2022-12-08 15:43:22 +0100

    Add ReferenceLinkConverter failing DeserializeInvalidType test

---
+++ tests/ServiceNow.Graph.Test/Serialization/ReferenceLinkConverterTests.cs
@@ -0,0 +1,35 @@
+﻿using ServiceNow.Graph.Exceptions;
+using ServiceNow.Graph.Models;
+using ServiceNow.Graph.Models.Helpers;
+using ServiceNow.Graph.Serialization;
+using Xunit;
+
+namespace ServiceNow.Graph.Test.Serialization
+{
+    public class ReferenceLinkConverterTests
+    {
+        private ReferenceLinkConverter converter;
+
+        public ReferenceLinkConverterTests()
+        {
+            this.converter = new ReferenceLinkConverter();
+        }
+
+        [Fact]
+        public void CanConvert_ReferenceLink()
+        {
+            Assert.True(this.converter.CanConvert(typeof(ReferenceLink)));
+        }
+
+        [Fact]
+        public void ReferenceLink_DeserializeInvalidType()
+        {
+            var json = "{\"link\":\"testLink\", value\":\"testValue\"}";
+            var serializer = new Serializer();
+            ServiceException exception = Assert.Throws<ServiceException>(() => serializer.DeserializeObject<ReferenceLink>(json));
+            Assert.Equal(ErrorConstants.Codes.GeneralException, exception.Error.ErrorDetail.Message);
+            // todo: FIX 
+            Assert.Equal(ErrorConstants.Messages.UnableToDeserializeDuration, exception.Error.ErrorDetail.DetailedMessage);
+        }
+    }
+}
